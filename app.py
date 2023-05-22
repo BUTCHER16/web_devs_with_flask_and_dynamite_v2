da3b1aa9e1f95e895fc2e9869fc48c516cf1dda6
Albert Ravidoss <ravidossalbert@gmail.com>
2023-05-22 03:13:20 -0700

    added a file called character.htm and linked that with the html

--- app.py
+++ app.py
@@ -2,9 +2,49 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+CHARACTER=[
+    {
+        'name' : 'Ross Geller',
+        'dia' : 'We were on a break!',
+        'pair': 'Ross-Rachel',
+        'be' : "The One With Unagi (Season 6, Episode 17)"
+    },
+    {
+        'name' : 'Chandler',
+        'dia' : "Hi, I’m Chandler. I make jokes when I’m uncomfortable.",
+        'pair': 'Mondler',
+        'be' : "The One With the Blackout"
+    },
+    {
+        'name' : 'Joey',
+        'dia' : "Joey doesn’t share food!",
+        'pair': 'Single',
+        'be' : "The One With Joey's Award (Season 7, Episode 18)"
+    },
+    {
+        'name' : 'Rachel',
+        'dia' : "Well, maybe I don’t need your money. Wait, wait, I said maybe!",
+        'pair': 'Ross-Rachel',
+        'be' : "The One Where Rachel Finds Out (Season 1, Episode 24)"
+    },
+    {
+        'name' : 'Monica',
+        'dia' : "Your little Harmonica is hammered.",
+        'pair': 'Mondler',
+        'be' : "THE ONE WITH ALL THE THANKSGIVINGS (SEASON 5, EPISODE 8)"
+    },
+    {
+        'name' : 'Pheobe',
+        'dia' : "See? He’s her lobster.",
+        'pair': 'Mike-Pheobe',
+        'be' : "The One Where Everyone Finds Out (Season 5, Episode 14)"
+    },
+    
+]
+
 @app.route("/")
 def greeting():
-    return render_template("home.html")
+    return render_template("home.html",character=CHARACTER)
 
 
 if __name__ == '__main__':
