3df73d7a7fcd0f89110f1cedb0efd2e7c8c0a739
ravidossalbert@gmail.com <albert ravidoss>
2023-05-27 11:48:01 +0530

    added  dynamic data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template ,jsonify
 
 app = Flask(__name__)
 
@@ -11,19 +11,19 @@ CHARACTER=[
     },
     {
         'name' : 'Chandler',
-        'dia' : "Hi, I’m Chandler. I make jokes when I’m uncomfortable.",
+        'dia' : "Hi, I'm Chandler. I make jokes when I'm uncomfortable.",
         'pair': 'Mondler',
         'be' : "The One With the Blackout"
     },
     {
         'name' : 'Joey',
-        'dia' : "Joey doesn’t share food!",
+        'dia' : "Joey doesn't share food!",
         'pair': 'Single',
         'be' : "The One With Joey's Award (Season 7, Episode 18)"
     },
     {
         'name' : 'Rachel',
-        'dia' : "Well, maybe I don’t need your money. Wait, wait, I said maybe!",
+        'dia' : "You Fell Asleep?",
         'pair': 'Ross-Rachel',
         'be' : "The One Where Rachel Finds Out (Season 1, Episode 24)"
     },
@@ -35,7 +35,7 @@ CHARACTER=[
     },
     {
         'name' : 'Pheobe',
-        'dia' : "See? He’s her lobster.",
+        'dia' : "My eyes! My eyes!.",
         'pair': 'Mike-Pheobe',
         'be' : "The One Where Everyone Finds Out (Season 5, Episode 14)"
     },
@@ -45,6 +45,10 @@ CHARACTER=[
 @app.route("/")
 def greeting():
     return render_template("home.html",character=CHARACTER)
+
+@app.route("/api/characters")
+def list_character():
+    return jsonify(CHARACTER)
 
 
 if __name__ == '__main__':
